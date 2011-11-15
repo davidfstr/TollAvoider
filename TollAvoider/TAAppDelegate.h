9cfb21d559b6eaa1ef1d4a24177d83ca98ca7db4
David Foster <davidfstr@gmail.com>
2011-11-15 14:52:59 -0800

    New project. Set deployment target to iOS 4.3. Opt-out of ARC. Add initial .gitignore.

---
+++ TollAvoider/TAAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  TAAppDelegate.h
+//  TollAvoider
+//
+//  Created by David Foster on 11/15/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TAAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ TollAvoider/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  TollAvoider
+//
+//  Created by David Foster on 11/15/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "TAAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([TAAppDelegate class]));
+    }
+}
